4aeea483482c8f88be94f7ebc3d6853f85b09b23
unknown <wkwk2805@gmail.com>
2020-04-24 11:36:35 +0900

    molecule처리

--- organisms/notifications/NotificationBody.js
+++ organisms/notifications/NotificationBody.js
@@ -1,21 +1,63 @@
 import React from "react";
 import NotificationInfo from "../../molecules/Information/NotificationInfo";
+import AvartarText from "../../molecules/AvartarText";
 
 const NotificationBody = () => {
   const container = { fontSize: 13, marginTop: 15, marginLeft: 5 };
   return (
     <div style={{ margin: 10 }}>
       <div style={container}>이번주</div>
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
       <div style={container}>이번달</div>
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
       <div style={container}>이전알림</div>
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
-      <NotificationInfo message="test1님이 어떠한 행동을 했습니다" />
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
+      <div style={{ marginTop: 5 }}>
+        <AvartarText
+          content="test1님이 어떠한 행동을 했습니다"
+          contentStyle={{ fontSize: 12, marginLeft: 10 }}
+          size={25}
+        />
+      </div>
     </div>
   );
 };
